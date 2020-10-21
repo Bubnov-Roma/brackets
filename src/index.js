f5815ae77e9c34ce846f7b126a2d93be9d04d152
Bubnov-Roma <kilkun@mail.ru>
2020-10-21 16:45:11 +0400

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+  if (str.length === 0 || undefined) return true; 
+  if (str.length % 2 !== 0) return false;
+
+  let open = "", close = "", stack = [str[0]]; 
+  
+  for (let el of bracketsConfig) {
+    open += el[0];
+    close += el[1]; }
+
+  for (let i = 1; i < str.length; i++) {
+    let currentBracket = str[i], lastBracket = stack[stack.length - 1];
+
+    if (close.includes(currentBracket) && close.indexOf(currentBracket) == open.indexOf(lastBracket)) {
+      stack.pop()  
+    }
+    else stack.push(currentBracket); 
+  }
+  return stack.length === 0;
+  };
